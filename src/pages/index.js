dfb700d84804f657cd368051203864610d83960c
Carlos Espinosa <carloespinosa88@gmail.com>
2021-11-12 20:20:00 -0500

    feat: add firebase to login with github

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,51 @@
-import { useState } from 'react'
+import Image from 'next/image'
+import { useState } from "react"
+import { getAuth, GithubAuthProvider, signOut } from "firebase/auth"
+import { loginWithGithub } from '../firebase/client'
 import Button from '../components/Button'
 import Input from '../components/Input'
 import SelectInput from '../components/SelectInput'
 import Checkbox from '../components/Checkbox'
 import { OPTIONS } from '../constanst'
 
-export default function Home() {
-  const [valueInput, setValueInput ] = useState('')
-  const [valueSelect, setValueSelect ] = useState('banana')
-  const [valueCheckbox, setValueCheckbox] = useState(false)
+const auth = getAuth();
 
-  function handleInput(event) {
-    setValueInput(event.target.value)
+export default function Home() {
+  const [userData, setUserData] = useState(null)
+  const [isSendedData, setIsSendedData] = useState(false)
+
+  const handleRedirectToGithub = () => {
+    loginWithGithub()
+      .then((result) => {
+        const credential = GithubAuthProvider.credentialFromResult(result);
+         if(credential) {
+           const token = credential.accessToken;
+           const user = result.user;
+           
+           setUserData(user)
+           console.log('AuthUserToken', token)           
+           console.log('AuthUser', user)
+         }
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        const credential = GithubAuthProvider.credentialFromError(error);
+        
+        console.log('AuthError', error)
+        console.log('AuthErrorMessages', errorMessage)
+      });
+  }
+
+  const logoutGithubAuth = () => {
+    signOut(auth).then(() => {
+      setUserData(null)
+      setIsSendedData(true)
+    }).catch((error) => {
+      const errorMessage = error.message;
+
+      console.log('LogoutError', errorMessage)
+    });
   }
 
   function handleCheckbox() {
@@ -22,57 +56,30 @@ export default function Home() {
     setValueSelect(event.target.value)
   }
 
-  const OPTIONS = [
-    {
-      label: "Apple",
-      value: "apple",
-    },
-    {
-      label: "Mango",
-      value: "mango",
-    },
-    {
-      label: "Banana",
-      value: "banana",
-    },
-    {
-      label: "Pineapple",
-      value: "pineapple",
-    },
-  ];
-
   return (
     <div>
-      <Button>Button</Button>
-      <Button loading={+true}>Button with long text</Button>
-      <Button loading={+false} disabled={true}>Disabled</Button>
-      <Input hasError errorMessage='Requerido' />
-      <Input 
-        required
-        value={valueInput}
-        label='Nombre completo'
-        placeholder='Placeholder'
-        onChange={handleInput}
-      />
-      <Input disabled value='Hola' />
-      <Checkbox 
-        id='20' 
-        label='React' 
-        error='Requerido'
-        onChange={handleCheckbox}
-        checked={valueCheckbox}
-      />
-      <Checkbox id='12' label='Angular' disabled/>
-      <SelectInput disabled options={OPTIONS} />
-      <SelectInput 
-        id='Select-id'
-        name='Frutas'
-        options={OPTIONS}
-        value={valueSelect}
-        onChange={handleSelect}
-        hasError
-        errorMessages="Requerido"
-      />
+      <Button
+        disabled={userData ? true : false}
+        onClick={handleRedirectToGithub}
+      >
+        Login
+      </Button>
+      <Button
+        disabled={userData ? false : true}
+        onClick={logoutGithubAuth}
+      >
+        Logout
+      </Button>
+      {userData && (
+        <>
+          <h4>User: {userData.displayName}</h4>
+          <h4>Email: {userData.email}</h4>
+          <Image height="300" width="300" src={userData.photoURL} alt='avatar-github' />
+        </>
+      )}
+      {isSendedData && !userData && (
+        <h2>Gracias por enviar tu informacion</h2>
+      )}
     </div>
   )
 }
